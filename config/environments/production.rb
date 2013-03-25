c47ff6533ea3bbd7ab6c23d30e8468944ed827b1
Prescott Paulin <prescott@your.vu>
2013-03-25 09:07:03 -0600

    Add User Model with Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ Omrails::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # in production, :host should be set to the actual host of the app
+  config.action_mailer.default_url_options = { :host => 'quiet-plateau-7068.herokuapp.com' }
 end
